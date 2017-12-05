5cf404e8bfa8486aa2917193ea69800f159503f1
Austin Chen <chenas@smu.edu>
2017-12-05 13:32:11 -0600

    Added graphs to TrackingViewController

--- Commotion/TrackingViewController.swift
+++ Commotion/TrackingViewController.swift
@@ -7,11 +7,68 @@
 //
 
 import UIKit
+import Charts
 
 class TrackingViewController: UIViewController {
-
+    
+    @IBOutlet weak var chartyBoi: LineChartView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+     
+    
+        let gradientColors = [UIColor.blue.cgColor, UIColor.cyan.cgColor] as CFArray // Colors of the gradient
+        let colorLocations:[CGFloat] = [1.0, 0.0] // Positioning of the gradient
+        let gradient = CGGradient.init(colorsSpace: CGColorSpaceCreateDeviceRGB(), colors: gradientColors, locations: colorLocations) // Gradient Object
+        
+        
+        
+        // Do any additional setup after loading the view.
+        let dollars1 = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        // 1 - creating an array of data entries
+        var yValues : [ChartDataEntry] = [ChartDataEntry]()
+        
+        for i in 0 ..< months.count {
+            yValues.append(ChartDataEntry(x: Double(i + 1), y: dollars1[i]))
+        }
+        
+        let data = LineChartData()
+        let ds = LineChartDataSet(values: yValues, label: "Months")
+        
+        ds.fill = Fill.fillWithLinearGradient(gradient!, angle: 90.0) // Set the Gradient
+        
+        ds.drawFilledEnabled = true // Draw the Gradient
+        
+        // Chart Styling
+        ds.mode = .cubicBezier
+        ds.drawCirclesEnabled = false
+        ds.drawValuesEnabled = false
+        
+        // Get rid of y axis stuff
+        chartyBoi.leftAxis.drawGridLinesEnabled = false
+        chartyBoi.rightAxis.drawGridLinesEnabled = false
+        chartyBoi.rightAxis.drawAxisLineEnabled = false
+        chartyBoi.rightAxis.drawLabelsEnabled = false
+        
+        // Get rid of legend and description
+        chartyBoi.chartDescription?.text = ""
+        chartyBoi.legend.enabled = false
+        
+        // X Axis
+        chartyBoi.xAxis.drawGridLinesEnabled = false
+        chartyBoi.xAxis.drawAxisLineEnabled = true
+        chartyBoi.xAxis.labelPosition = XAxis.LabelPosition.bottom
+        chartyBoi.xAxis.drawLabelsEnabled = false
+        
+        
+        data.addDataSet(ds)
+        self.chartyBoi.data = data
+        
+        
+        
+        
 
         // Do any additional setup after loading the view.
     }

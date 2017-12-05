848874adf87c1364126629e917ef04eb4bd58c46
Kellen Schmidt <kellenschmidt31@gmail.com>
2017-12-05 10:59:56 -0600

    Added initial page view controller stuff

---
+++ Commotion/PageViewController.swift
@@ -0,0 +1,72 @@
+//
+//  PageViewController.swift
+//  Commotion
+//
+//  Created by Kellen Schmidt on 12/5/17.
+//  Copyright © 2017 Eric Larson. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController, UIPageViewControllerDataSource {
+    
+    var arrPageTitle: NSArray = NSArray()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        arrPageTitle = ["This is The App Guruz", "This is Table Tennis 3D", "This is Hide Secrets"];
+        self.dataSource = self
+        self.setViewControllers([getViewControllerAtIndex(index: 0)] as [UIViewController], direction: UIPageViewControllerNavigationDirection.forward, animated: false, completion: nil)
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
+    {
+        let pageContent: PageContentViewController = viewController as! PageContentViewController
+        var index = pageContent.pageIndex
+        if ((index == 0) || (index == NSNotFound))
+        {
+            return nil
+        }
+        index = index - 1
+        return getViewControllerAtIndex(index: index)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController?
+    {
+        let pageContent: PageContentViewController = viewController as! PageContentViewController
+        var index = pageContent.pageIndex
+        if (index == NSNotFound)
+        {
+            return nil
+        }
+        index = index + 1
+        if (index == arrPageTitle.count)
+        {
+            return nil;
+        }
+        return getViewControllerAtIndex(index: index)
+    }
+    
+    func getViewControllerAtIndex(index: NSInteger) -> PageContentViewController
+    {
+        // Create a new view controller and pass suitable data.
+        let pageContentViewController = self.storyboard?.instantiateViewController(withIdentifier: "PageContentViewController") as! PageContentViewController
+        pageContentViewController.strTitle = "\(arrPageTitle[index])"
+        pageContentViewController.pageIndex = index
+        return pageContentViewController
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

b37497ba487e2be0f8a87ccb3b89df612f0a5055
Logan Dorsey <logandorsey@gmail.com>
2017-12-06 11:48:11 -0600

    Made some changes

--- Commotion/NightyNight.swift
+++ Commotion/NightyNight.swift
@@ -8,8 +8,36 @@
 
 import Foundation
 
-class NightyNight : NSObject
+class NightyNight : NSObject, NSCoding
 {
+    
+    struct PropertyKey {
+        static let eventStart = "eventStart"
+        static let eventEnd = "eventEnd"
+        static let alarmTime = "alarmTime"
+        static let motionEvents = "MotionEvents"
+        static let lengthOfSleep = "lengthOfSleep"
+        static let sleepScore = "sleepScore"
+        static let deepSleepPercentage = "deepSleepPercentage"
+        static let lightSleepPercentage = "lightSleepPercentage"
+        static let awakePercentage = "awakePercentage"
+    }
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(eventStart, forKey: PropertyKey.eventStart)
+        aCoder.encode(eventEnd, forKey: PropertyKey.eventEnd)
+        aCoder.encode(alarmTime, forKey: PropertyKey.alarmTime)
+        aCoder.encode(motionEvents, forKey: PropertyKey.motionEvents)
+        aCoder.encode(lengthOfSleep, forKey: PropertyKey.lengthOfSleep)
+        aCoder.encode(sleepScore, forKey: PropertyKey.sleepScore)
+        aCoder.encode(deepSleepPercentage, forKey: PropertyKey.deepSleepPercentage)
+        aCoder.encode(lightSleepPercentage, forKey: PropertyKey.lightSleepPercentage)
+        aCoder.encode(awakePercentage, forKey: PropertyKey.awakePercentage)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        <#code#>
+    }
+    
     // raw data values
     var eventStart = Date()
     var eventEnd = Date()

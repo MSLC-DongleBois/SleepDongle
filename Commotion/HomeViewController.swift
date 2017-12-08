f4d97ac59e3e41ad1375989f31429dc3de8af229
Kellen Schmidt <kellenschmidt31@gmail.com>
2017-12-08 06:02:31 -0600

    Moved sample data to homeVC and added dynamic histogram xaxis labels

--- Commotion/HomeViewController.swift
+++ Commotion/HomeViewController.swift
@@ -15,6 +15,8 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        saveSampleNights()
 
         // Do any additional setup after loading the view.
         startTrackingButton.layer.cornerRadius = 4
@@ -59,6 +61,40 @@ class HomeViewController: UIViewController {
         
         // show the alert
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    private func saveSampleNights()
+    {
+        let night = NightWatchman()
+        var temp: NightyNight = NightyNight(start: Date(timeIntervalSinceNow: -100000), end: Date(timeIntervalSinceNow: -80000), alarm: Date(), events: [20, 15, 10, 10, 9, 11, 7, 9, 14, 11, 6, 10, 10, 10, 10, 10, 10, 12, 8, 11, 9, 7, 6, 8, 8, 8, 5, 6, 7, 4, 3, 15, 2, 4, 5, 4, 0, 0, 1, 0, 2, 3, 2, 0, 4, 0, 1, 4], length: DateInterval(), score: 100, deepPer: 13.64, lightPer: 52.55, awakePer: 33.81, classification: 1)
+        temp = SleepyBoye.analyzeNight(data: temp)
+        night.nights.append(temp)
+        
+        temp = NightyNight(start: Date(timeIntervalSinceNow: -50000), end: Date(timeIntervalSinceNow: -5000), alarm: Date(), events: [6, 7, 8, 9, 10, 11, 10, 10, 9, 9, 10, 12, 13, 14, 16, 18, 20, 19, 20, 10, 4, 4, 3, 4, 10, 9, 9, 10, 10, 12, 11, 11, 11, 9, 8, 9, 11, 12, 14, 13, 14, 14, 11, 10, 10, 12, 13, 15], length: DateInterval(), score: 1, deepPer: 71, lightPer: 22.90, awakePer: 6.1, classification: 2)
+        temp = SleepyBoye.analyzeNight(data: temp)
+        night.nights.append(temp)
+        
+        temp = NightyNight(start: Date(), end: Date(), alarm: Date(), events: [1, 2, 3], length: DateInterval(), score: 33, deepPer: 69.69, lightPer: 69.69, awakePer: 69.69, classification: 3)
+        temp = SleepyBoye.analyzeNight(data: temp)
+        night.nights.append(temp)
+        
+        temp = NightyNight(start: Date(), end: Date(), alarm: Date(), events: [1, 2, 3], length: DateInterval(), score: 55, deepPer: 69.69, lightPer: 69.69, awakePer: 69.69, classification: 3)
+        temp = SleepyBoye.analyzeNight(data: temp)
+        night.nights.append(temp)
+        
+        temp = NightyNight(start: Date(), end: Date(), alarm: Date(), events: [1, 2, 3], length: DateInterval(), score: 18, deepPer: 69.69, lightPer: 69.69, awakePer: 69.69, classification: 3)
+        temp = SleepyBoye.analyzeNight(data: temp)
+        night.nights.append(temp)
+        
+        temp = NightyNight(start: Date(), end: Date(), alarm: Date(), events: [1, 2, 3], length: DateInterval(), score: 97, deepPer: 69.69, lightPer: 69.69, awakePer: 69.69, classification: 3)
+        temp = SleepyBoye.analyzeNight(data: temp)
+        night.nights.append(temp)
+        
+        temp = NightyNight(start: Date(), end: Date(), alarm: Date(), events: [9,9,8,9,0,8,9,8,7,5,5,5,4,3,5,4,3,4,5,6,4,3,2,2,1,2,3,2,1,1,1,2,1,2,2,1,2,1,2,1,2,3,4,1,1,1,2,3,3,7,6,5,4,3,2,3,2,4,5,7,8,9,0,0,0,9,8,7,6,6,5,4,5,6,7,8,6,5,5,7,7,8], length: DateInterval(), score: 97, deepPer: 69.69, lightPer: 69.69, awakePer: 69.69, classification: 3)
+        temp = SleepyBoye.analyzeNight(data: temp)
+        night.nights.append(temp)
+        
+        night.saveNights()
     }
 
 }
